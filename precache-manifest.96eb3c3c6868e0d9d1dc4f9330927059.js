64784382a0a245bf8290ec25d0e35e1b0fa36a67
Jisan Zaman <jisan.zaman@dynamo.works>
2020-04-15 20:13:09 -0400

    Updates

---
+++ precache-manifest.96eb3c3c6868e0d9d1dc4f9330927059.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35bdfcd6729c4f17b8a15d8f8756a0dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e5da8f486b6aaaa7f13",
+    "url": "/static/js/2.ed4ec856.chunk.js"
+  },
+  {
+    "revision": "46a6d6e633779a6b76d68db678882bed",
+    "url": "/static/js/2.ed4ec856.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0844fc94838d4293d580",
+    "url": "/static/js/main.1eac5f61.chunk.js"
+  },
+  {
+    "revision": "826b4605b8b7fad01c4f",
+    "url": "/static/js/runtime-main.ce417e0b.js"
+  }
+]);
