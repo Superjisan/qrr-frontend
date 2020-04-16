17c6490efa5e3eb0ab1d260589987079b3f3c4e5
Jisan Zaman <jisan.zaman@dynamo.works>
2020-04-15 21:20:57 -0400

    fix linking

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, createHistory } from 'react-router-dom';
+import { Router, Route} from 'react-router-dom';
 
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-// import Link from '@material-ui/core/Link';
+import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -60,7 +60,9 @@ const NavigationNonAuth = () => {
             QR^2
           </Typography>
           <Typography>
-            <Button color="primary.contrastText" href={`${routes.SIGN_IN}`}>Sign In</Button>
+            <Link to={routes.SIGN_IN}>
+              <Button color="primary.contrastText">Sign In</Button>
+            </Link>
           </Typography>
         </Toolbar>
       </AppBar>
