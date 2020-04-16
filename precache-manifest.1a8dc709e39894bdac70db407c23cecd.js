b8518bc5a8f6ad712aa2efb99344faa70edc3e19
Jisan Zaman <jisan.zaman@dynamo.works>
2020-04-15 22:15:27 -0400

    Updates

---
+++ precache-manifest.1a8dc709e39894bdac70db407c23cecd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d23fbcfa76655021f68d4bace955f20",
+    "url": "/qrr-frontend/index.html"
+  },
+  {
+    "revision": "3c3c5170b62d4f5d860e",
+    "url": "/qrr-frontend/static/js/2.7d2ecb88.chunk.js"
+  },
+  {
+    "revision": "46a6d6e633779a6b76d68db678882bed",
+    "url": "/qrr-frontend/static/js/2.7d2ecb88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "226b4e80fa403cb84e90",
+    "url": "/qrr-frontend/static/js/main.55b682b2.chunk.js"
+  },
+  {
+    "revision": "676241d4ac79a70859f2",
+    "url": "/qrr-frontend/static/js/runtime-main.8887fb40.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/qrr-frontend/precache-manifest.2710b454e4a450c7a877c1d4f5c4e705.js"
+  "/qrr-frontend/precache-manifest.1a8dc709e39894bdac70db407c23cecd.js"
 );
 
 self.addEventListener('message', (event) => {
