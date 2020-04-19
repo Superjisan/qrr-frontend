612bd958a871e36f6420ce247b0d5e71e1117be0
Jisan Zaman <jisan.zaman@dynamo.works>
2020-04-19 07:57:06 -0400

    Updates

---
+++ precache-manifest.978f96a3e54ff3614e503a6303f34abb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd136921b8ac0d8438cac026d0adc57f",
+    "url": "/qrr-frontend/index.html"
+  },
+  {
+    "revision": "2dd5e831bfbf4c3c4a19",
+    "url": "/qrr-frontend/static/js/2.96a470fc.chunk.js"
+  },
+  {
+    "revision": "e510e42b7821ada5d5935abb8c38956d",
+    "url": "/qrr-frontend/static/js/2.96a470fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b90faa83a26275ec0c",
+    "url": "/qrr-frontend/static/js/main.83800ff8.chunk.js"
+  },
+  {
+    "revision": "676241d4ac79a70859f2",
+    "url": "/qrr-frontend/static/js/runtime-main.8887fb40.js"
+  }
+]);
