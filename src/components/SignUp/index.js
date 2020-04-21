bd54269dd965e7a7bd001f49b3d3a61c394087cd
Jisan Zaman <jisan.zaman@dynamo.works>
2020-04-21 00:30:37 -0400

    update signup page

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,7 +1,12 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+
+import { withStyles, Typography } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 
 import * as routes from '../../constants/routes';
 import ErrorMessage from '../Error';
@@ -14,102 +19,149 @@ const SIGN_UP = gql`
   }
 `;
 
-const INITIAL_STATE = {
-  username: '',
-  email: '',
-  password: '',
-  passwordConfirmation: '',
-};
+const useStyles = (theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '100%'
+    }
+  },
+  textField: {
+    marginTop: 10,
+    marginBottom: 10,
+    width: `100%`
+  }
+});
 
-const SignUpPage = ({ history, refetch }) => (
-  <div>
-    <h1>SignUp</h1>
-    <SignUpForm history={history} refetch={refetch} />
-  </div>
+const SignUpPage = ({ history, refetch, classes }) => (
+  <Container maxWidth="sm">
+    <Typography
+      align="center"
+      variant="h3"
+      classes={classes.textField}
+    >
+      Sign Up
+    </Typography>
+    <SignUpForm
+      history={history}
+      refetch={refetch}
+      classes={classes}
+    />
+  </Container>
 );
 
-class SignUpForm extends Component {
-  state = { ...INITIAL_STATE };
+const SignUpForm = (props) => {
+  const { classes } = props;
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState(
+    ''
+  );
 
-  onChange = event => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
+  const onUsernameChange = (event) => {
+    const { value } = event.target;
+    setUsername(value);
   };
 
-  onSubmit = (event, signUp) => {
-    signUp().then(async ({ data }) => {
-      this.setState({ ...INITIAL_STATE });
+  const onEmailChange = (event) => {
+    const { value } = event.target;
+    setEmail(value);
+  };
 
+  const onPasswordChange = (event) => {
+    const { value } = event.target;
+    setPassword(value);
+  };
+
+  const onPasswordConfirmationChange = (event) => {
+    const { value } = event.target;
+    setPasswordConfirmation(value);
+  };
+
+  const onSubmit = (event, signUp) => {
+    signUp().then(async ({ data }) => {
       localStorage.setItem('token', data.signUp.token);
 
-      await this.props.refetch();
+      await props.refetch();
 
-      this.props.history.push(routes.LANDING);
+      props.history.push(routes.LANDING);
     });
 
     event.preventDefault();
   };
 
-  render() {
-    const {
-      username,
-      email,
-      password,
-      passwordConfirmation,
-    } = this.state;
+  const isInvalid =
+    password !== passwordConfirmation ||
+    password === '' ||
+    email === '' ||
+    username === '';
 
-    const isInvalid =
-      password !== passwordConfirmation ||
-      password === '' ||
-      email === '' ||
-      username === '';
+  return (
+    <Mutation
+      mutation={SIGN_UP}
+      variables={{ username, email, password }}
+    >
+      {(signUp, { data, loading, error }) => (
+        <form onSubmit={(event) => onSubmit(event, signUp)}>
+          <TextField
+            className={classes.textField}
+            required
+            variant="outlined"
+            className={classes.textField}
+            name="username"
+            value={username}
+            onChange={onUsernameChange}
+            type="text"
+            placeholder="username"
+          />
+          <TextField
+            className={classes.textField}
+            required
+            variant="outlined"
+            className={classes.textField}
+            name="email"
+            value={email}
+            onChange={onEmailChange}
+            type="text"
+            placeholder="Email Address"
+          />
+          <TextField
+            className={classes.textField}
+            required
+            variant="outlined"
+            className={classes.textField}
+            name="password"
+            value={password}
+            onChange={onPasswordChange}
+            type="password"
+            placeholder="Password"
+          />
+          <TextField
+            className={classes.textField}
+            required
+            variant="outlined"
+            className={classes.textField}
+            name="passwordConfirmation"
+            value={passwordConfirmation}
+            onChange={onPasswordConfirmationChange}
+            type="password"
+            placeholder="Confirm Password"
+          />
+          <Button
+            variant="contained"
+            disabled={isInvalid || loading}
+            type="submit"
+          >
+            Sign Up
+          </Button>
 
-    return (
-      <Mutation
-        mutation={SIGN_UP}
-        variables={{ username, email, password }}
-      >
-        {(signUp, { data, loading, error }) => (
-          <form onSubmit={event => this.onSubmit(event, signUp)}>
-            <input
-              name="username"
-              value={username}
-              onChange={this.onChange}
-              type="text"
-              placeholder="Full Name"
-            />
-            <input
-              name="email"
-              value={email}
-              onChange={this.onChange}
-              type="text"
-              placeholder="Email Address"
-            />
-            <input
-              name="password"
-              value={password}
-              onChange={this.onChange}
-              type="password"
-              placeholder="Password"
-            />
-            <input
-              name="passwordConfirmation"
-              value={passwordConfirmation}
-              onChange={this.onChange}
-              type="password"
-              placeholder="Confirm Password"
-            />
-            <button disabled={isInvalid || loading} type="submit">
-              Sign Up
-            </button>
-
-            {error && <ErrorMessage error={error} />}
-          </form>
-        )}
-      </Mutation>
-    );
-  }
-}
+          {error && <ErrorMessage error={error} />}
+        </form>
+      )}
+    </Mutation>
+  );
+};
 
 const SignUpLink = () => (
   <p>
@@ -117,6 +169,8 @@ const SignUpLink = () => (
   </p>
 );
 
-export default withRouter(SignUpPage);
+export default withStyles(useStyles, { withTheme: true })(
+  withRouter(SignUpPage)
+);
 
 export { SignUpForm, SignUpLink };
